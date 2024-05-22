521f16da731004a147a57d1555d5351a0069c59c
gangabiswa <Gbiswa@pgh.year.org>
2024-05-22 15:39:45 -0400

    added lease contract and worked in it

--- src/main/java/com/pluralsight/Contract.java
+++ src/main/java/com/pluralsight/Contract.java
@@ -1,5 +1,10 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 public abstract class Contract {
     private String dateOfContract;
     private String CustomerName;
@@ -36,8 +41,23 @@ public abstract class Contract {
     public void setVehicleSold(int vehicleSold) {
         VehicleSold = vehicleSold;
     }
+    public void appendToFile() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("contracts.csv", true))) {
+            writer.write(this.toString());
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-    public abstract double getTotalPrice();
-
-    public abstract double getMonthlyPayment();
+    @Override
+    public String toString() {
+        return "Contract{" +
+                "dateOfContract='" + dateOfContract + '\'' +
+                ", CustomerName='" + CustomerName + '\'' +
+                ", VehicleSold=" + VehicleSold +
+                ", totalPrice=" + totalPrice +
+                ", monthlyPayment=" + monthlyPayment +
+                '}';
+    }
 }
--- src/main/java/com/pluralsight/LeaseContract.java
+++ src/main/java/com/pluralsight/LeaseContract.java
@@ -1,5 +1,73 @@
 package com.pluralsight;
 
 public class LeaseContract extends Contract{
+    private double originalPrice;
+    private double endingValue;
+    private double leaseFee;
+    private double interRate;
+    private int leaseTermMonths;
+
+    public LeaseContract(String dateOfContract, String customerName, int vehicleSold, double originalPrice, double endingValue, double leaseFee, double interRate, int leaseTermMonths) {
+        super(dateOfContract, customerName, vehicleSold);
+        this.originalPrice = originalPrice;
+        this.endingValue = originalPrice* 0.50;
+        this.leaseFee = originalPrice * 0.07;
+        this.interRate = 0.04;
+        this.leaseTermMonths = 36;
+    }
+
+    public double getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(double originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public double getEndingValue() {
+        return endingValue;
+    }
+
+    public void setEndingValue(double endingValue) {
+        this.endingValue = endingValue;
+    }
+
+    public double getLeaseFee() {
+        return leaseFee;
+    }
+
+    public void setLeaseFee(double leaseFee) {
+        this.leaseFee = leaseFee;
+    }
+
+    public double getInterRate() {
+        return interRate;
+    }
+
+    public void setInterRate(double interRate) {
+        this.interRate = interRate;
+    }
+
+    public int getLeaseTermMonths() {
+        return leaseTermMonths;
+    }
+
+    public void setLeaseTermMonths(int leaseTermMonths) {
+        this.leaseTermMonths = leaseTermMonths;
+    }
+    @Override
+    public double getTotalPrice() {
+        return (getVehicleSold() - endingValue) + leaseFee;
+    }
+
+    @Override
+    public double getMonthlyPayment() {
+        int numberOfPayments = 36;
+        double interestRate = 4.0 / 1200;
+        double monthlyPayment = getTotalPrice() * (interestRate * Math.pow(1 + interestRate, numberOfPayments)) / (Math.pow(1 + interestRate, numberOfPayments) - 1);
+        monthlyPayment = Math.round(monthlyPayment * 100);
+        monthlyPayment /= 100;
+        return monthlyPayment;
+    }
 
 }
