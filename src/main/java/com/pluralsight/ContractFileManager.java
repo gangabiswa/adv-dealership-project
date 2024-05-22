0143f38a05281369723d1ac8427fcbe72bdf6c57
gangabiswa <Gbiswa@pgh.year.org>
2024-05-22 15:45:37 -0400

    added and worked on contractfilemanager

--- src/main/java/com/pluralsight/ContractFileManager.java
+++ src/main/java/com/pluralsight/ContractFileManager.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ContractFileManager {
+    private static final String FILE_NAME = "contracts.csv";
+
+    private  void saveContract(Contract contract){
+        try (BufferedWriter writer =new BufferedWriter(new FileWriter(FILE_NAME, true))){
+            writer.write(contract.toString());
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
